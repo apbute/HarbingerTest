5b6deca4204649e96e0f8e984676c018e0a05854
apbute <39114707+apbute@users.noreply.github.com>
2024-09-11 23:00:24 +0530

    Initial commit

---
+++ HarbingerTest/HarbingerTest/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using HarbingerTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HarbingerTest.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            // For demo I am using hardcoded credentials
+            if (userLogin.Username == "Ankush" && userLogin.Password == "Harbinger")
+            {
+                var token = GenerateJwtToken(userLogin.Username);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized("Invalid credentials");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.ExpirationMinutes),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
